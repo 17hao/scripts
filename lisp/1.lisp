40b42674814e7297cb73287485ccd69aa8eef70b
17hao <sqh1107@gmail.com>
2020-04-28 10:11:29 +0800

    lisp

--- lisp/1.lisp
+++ lisp/1.lisp
@@ -1,4 +1,20 @@
 (defun triple (x)
   (* 3 x))
+(print (triple -5))
 
-(princ (triple -5))
+;; (define (square x) (* x x))
+(defun square (x) (* x x))
+(print (square 5))
+
+(defun myabs (x)
+  (if (< x 0)
+      (- x)
+      x))
+(print (myabs -5))
+
+;; local variable
+(let ((size 2)) (print (* size 5)))
+
+;; dynamic variable
+(defparameter *size* 2)
+(print *size*)
