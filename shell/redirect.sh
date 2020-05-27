26fef38aab1cfe6bb4311c526e4c029db02d8ab5
17hao <sqh1107@gmail.com>
2020-05-27 23:44:13 +0800

    update redirect

--- shell/redirect.sh
+++ shell/redirect.sh
@@ -7,3 +7,12 @@ else
     today=$(date +%y%m%d)
     ls -al . >log.$today
 fi
+
+ls -0 > redirect.log 2>&1
+
+ls -al &> redirect.log # semantically equivalent to above command
+
+
+ps -0 2>&1 # redirect strerr(fd:1) to stdout(fd:2)
+
+pa >/dev/null 2>&1 # redirect stderr and stdout to /dev/null(black hole)
