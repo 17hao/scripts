7fea60d4fb6a3f365ee7b28068413d90bb657fcc
17hao <sqh1107@gmail.com>
2020-05-28 16:47:06 +0800

    update

--- shell/arguments.sh
+++ shell/arguments.sh
@@ -3,3 +3,5 @@ BASE_PATH=$(dirname $(dirname $0)) # $0 is the name of script
 echo "The value of variable \$BASE_PATH is:" $BASE_PATH
 
 echo "intput arg is:" $@ # $@ is arguments
+
+echo ${@:2} # all arguments except the first one
